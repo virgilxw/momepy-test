0c06ea4fe3cacf12c33ebd585d442c8f369a1b14
reuben <reubenwxw@gmail.com>
2023-04-27 23:17:39 +0100

    mapbox tile uploader done
    Former-commit-id: 80e556d4590d328d19ab4becde345426aaee7ff8

--- run_dask.py
+++
@@ -1,7 +0,0 @@
-from dask.distributed import Client, LocalCluster
-
-daskCluster = LocalCluster(threads_per_worker=2,
-                n_workers=8, memory_limit='100GB',
-                dashboard_address=':8787')
-
-client = Client(daskCluster)
