bd6f54e3ff327ddcd0051ac64a44ea693d43ffe5
reuben <reubenwxw@gmail.com>
2023-06-23 22:11:56 +0100

    nestedCelData works

--- website/morphometrics-explorer/components/layout/Sidebar.tsx
+++ website/morphometrics-explorer/components/layout/Sidebar.tsx
@@ -33,7 +33,7 @@ const ViolinPlot = ({ city_data, width, height, plotKey, targetValue }) => {
 
     return (
         <svg style={{ width: width, height: height * 2 }}>
-            <g transform={`translate(${width *0.1}, ${(height * 2) / 2})`}>
+            <g transform={`translate(${width * 0.1}, ${(height * 2) / 2})`}>
                 <ViolinShape
                     height={height}
                     xScale={xScale}
@@ -49,21 +49,49 @@ const ViolinPlot = ({ city_data, width, height, plotKey, targetValue }) => {
 const Sidebar = ({ city_data, selectedCell, setSelectedCell }) => {
 
     const sidebarWidth = 266
+    const [nestedCellData, setNestedCellData] = useState({});
+
+    useEffect(() => {
+        if (!Object.keys(selectedCell).includes("nothing_selected")) {
+            let nestedObject = {};
+
+            for (const key in selectedCell) {
+                const regex = /_(25|50|75)$/;
+                const match = key.match(regex);
+
+                if (match) {
+                    const subkey = key.replace(regex, '');
+                    if (!nestedObject[subkey]) {
+                        nestedObject[subkey] = {};
+                    }
+                    nestedObject[subkey][match[1]] = selectedCell[key];
+                } else {
+                    if (!nestedObject[key]) {
+                        nestedObject[key] = {};
+                    }
+                    nestedObject[key]["base"] = selectedCell[key];
+                }
+            }
+
+            setNestedCellData(nestedObject);
+            console.log(nestedObject)
+        }
+    }, [selectedCell]);
 
     return (
         <div className="sidebar shadow-md bg-zinc-50 overflow-y-auto">
             {
-                selectedCell && Object.keys(selectedCell).length > 0 ?
-                    Object.entries(selectedCell).map(([key, value]) => (
+                nestedCellData && Object.keys(nestedCellData).length > 0 ?
+                    Object.entries(nestedCellData).map(([key, value]) => (
                         <div key={key} className="p-4 border mb-4">
                             <h3 className="text-lg font-bold mb-2">{key}</h3>
-                            <p>{value}</p>
+                            <p>{value["base"]}</p>
                             <ViolinPlot
                                 city_data={city_data["singapore"]}
                                 width={sidebarWidth}
                                 height={50}
                                 plotKey={key}
-                                targetValue={value}
+                                targetValue={value["base"]}
                             />
                         </div>
                     )) : null
