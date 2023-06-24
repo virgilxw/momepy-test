15ffda17c4300b2129a828ee975c429bfa29f4c1
reuben <reubenwxw@gmail.com>
2023-06-24 22:21:59 +0100

    uID and cluster now seperated and displayed sepera
    Former-commit-id: 5e503f68556e11bcea2847c68380b106f80024e8

--- website/morphometrics-explorer/components/layout/Layout.tsx
+++ website/morphometrics-explorer/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ const Layout = (props: PropsWithChildren) => {
       </div>
 
       <div className="grid md:grid-cols-sidebar ">
-        <Sidebar selectedCell = {props.selectedCell} setSelectedCell = {props.setSelectedCell} city_data = {props.city_data}/>
+        <Sidebar selectedCell = {props.selectedCell} setSelectedCell = {props.setSelectedCell} city_data = {props.city_data} clusterID = {props.clusterID} setclusterID = {props.setclusterID} />
         {props.children}
       </div>
     </div>
--- website/morphometrics-explorer/pages/_app.tsx
+++ website/morphometrics-explorer/pages/_app.tsx
@@ -7,9 +7,10 @@ import React, { useState, useEffect, useRef } from "react";
 function MyApp({ Component, pageProps }: AppProps) {
 
   const [selectedCell, setSelectedCell] = useState({ nothing_selected: "Select a Cell to Continue" });
+  const [clusterID, setclusterID] = useState(0);
 
   return (
-    <Component selectedCell={selectedCell} setSelectedCell={setSelectedCell} {...pageProps} />
+    <Component selectedCell={selectedCell} setSelectedCell={setSelectedCell} clusterID={clusterID} setclusterID={setclusterID} {...pageProps} />
   );
 }
 export default MyApp;
