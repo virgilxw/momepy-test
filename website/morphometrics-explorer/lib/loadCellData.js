d8e6ed11a7e4185aa475d1f5439ca48002d9d947
reuben <reubenwxw@gmail.com>
2023-07-01 20:29:11 +0100

    modified to break tess csv into smaller chunks

--- website/morphometrics-explorer/lib/loadCellData.js
+++ website/morphometrics-explorer/lib/loadCellData.js
@@ -11,7 +11,7 @@ export async function loadCellData() {
 
     let output = {}
 
-    const allPostsData = fileNames.map((fileName) => {
+    fileNames.forEach((fileName) => {
         // Read markdown file as string
         const fullPath = path.join(postsDirectory, fileName);
         const fileContents = fs.readFileSync(fullPath, 'utf8')
@@ -19,9 +19,8 @@ export async function loadCellData() {
         let output_1 = {}
         Papa.parse(fileContents, {
             header: true,
-            complete: function (fileContents) {
-                const output = {};
-                fileContents.data.forEach(row => {
+            complete: function (results) {
+                results.data.forEach(row => {
                     for (const key in row) {
                         if (!output_1[key]) output_1[key] = [];
                         output_1[key].push(row[key]);
@@ -31,23 +30,20 @@ export async function loadCellData() {
             }
         })
 
-        // const object = new Jenks(output_1["tess_covered_area"].filter(item => typeof item === 'number'), 8).naturalBreak()
+        let city_name = fileName.split("-", 1)[0]
 
-        function filterNumbers(data) {
-            const numbers = [];
-            for (const item of data) {
-                try {
-                    const number = parseFloat(item);
-                    numbers.push(number);
-                } catch (error) {
-                    // Ignore elements that are not numbers
-                }
+        if (city_name in output){
+            for (const key in output_1){
+                output[city_name][key] = (output[city_name][key] || []).concat(output_1[key])
             }
-            return numbers;
+        } else {
+            output[city_name] = output_1
         }
-
-        output[fileName.slice(0, -4)] = _.mapValues(output_1, list => _.sampleSize(list, 400))
     })
+
+    for (const key in output){
+        output[key] = _.mapValues(output[key], list => _.sampleSize(list, 600))
+    }
 
     return output
 }
